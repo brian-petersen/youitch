96f5abedfd87dfd3e7a4bfd26c2fc992d5550a34
Brian Petersen <skiwalk3r@gmail.com>
2024-08-17 13:22:34 -0600

    Adds prettier and formats code

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode,
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
