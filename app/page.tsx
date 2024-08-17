2ab155c371db232cd178f03f610a915939ca7b18
Brian Petersen <skiwalk3r@gmail.com>
2024-08-17 13:19:01 -0600

    Adds tabs for minimized content

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,14 @@
 
 import { useEffect, useState } from "react"
 
-type Dispatch = (show: boolean) => void
-
 type SectionProps = {
   title: string,
-  show: boolean,
   onHide: () => void,
-  onShow: () => void,
   className?: string,
   children: React.ReactNode,
 }
 
-function Section({ title, show, className, onHide, onShow, children }: SectionProps) {
-  if (!show) {
-    return <p onClick={onShow}>hidden</p>
-  }
-
+function Section({ title, className, onHide, children }: SectionProps) {
   return (
     <div className={`${className} border rounded flex flex-col`}>
       <div className="py-1.5 px-2 flex border-b">
@@ -29,6 +21,22 @@ function Section({ title, show, className, onHide, onShow, children }: SectionPr
   )
 }
 
+type FooterButtonProps = {
+  title: string,
+  onClick: () => void,
+}
+
+function FooterButton({ title, onClick }: FooterButtonProps) {
+  return <button
+    className="text-white bg-indigo-500 hover:opacity-70 py-2 px-3 rounded-t"
+    onClick={onClick}
+  >
+    {title}
+  </button>
+}
+
+type Dispatch = (show: boolean) => void
+
 export default function Home() {
   // TODO improve how the hostname is loaded (don't render anything until it's ready maybe?)
   let [hostname, setHostname] = useState<string | null>(null)
@@ -38,7 +46,7 @@ export default function Home() {
   }, [])
 
   const twitchHandle = "emongg"
-  const youtubeVideo = "7EiK8PMccwQ"
+  const youtubeVideo = "pjcR6O8BES8"
 
   const [showYoutubeVideo, setShowYoutubeVideo] = useState(true)
   const [showTwitchVideo, setShowTwitchVideo] = useState(true)
@@ -55,66 +63,70 @@ export default function Home() {
   }
 
   return (
-    <div className="h-screen flex gap-2 p-2">
-      <div className="grow flex flex-col gap-2">
-        <Section
-          title="YouTube Video"
-          show={showYoutubeVideo}
-          onHide={handleHide(setShowYoutubeVideo)}
-          onShow={handleShow(setShowYoutubeVideo)}
-          className="grow"
-        >
-          <iframe
-            className="h-full w-full"
-            src={`https://www.youtube.com/embed/${youtubeVideo}`}
-            allowFullScreen
+    <div className="flex flex-col h-screen">
+      <div className="grow flex gap-2 p-2">
+        <div className="grow flex flex-col gap-2">
+        {showYoutubeVideo &&<Section
+            title="YouTube Video"
+            onHide={handleHide(setShowYoutubeVideo)}
+            className="grow"
           >
-          </iframe>
-        </Section>
+            <iframe
+              className="h-full w-full"
+              src={`https://www.youtube.com/embed/${youtubeVideo}`}
+              allowFullScreen
+            >
+            </iframe>
+          </Section>
+        }
 
-        <Section
-          title="Twitch Video"
-          show={showTwitchVideo}
-          onHide={handleHide(setShowTwitchVideo)}
-          onShow={handleShow(setShowTwitchVideo)}
-          className="grow"
+        {showTwitchVideo && <Section
+            title="Twitch Video"
+            onHide={handleHide(setShowTwitchVideo)}
+            className="grow"
+          >
+            <iframe
+              className="h-full w-full"
+              src={`https://player.twitch.tv/?channel=${twitchHandle}&autoplay=false&parent=${hostname}`}
+              allowFullScreen
+            >
+            </iframe>
+          </Section>}
+        </div>
+
+        {showTwitchChat && <Section
+          title="Twitch Chat"
+          onHide={handleHide(setShowTwitchChat)}
         >
           <iframe
-            className="h-full w-full"
-            src={`https://player.twitch.tv/?channel=${twitchHandle}&autoplay=false&parent=${hostname}`}
-            allowFullScreen
+            className="h-full"
+            width="350"
+            src={`https://www.twitch.tv/embed/${twitchHandle}/chat?parent=${hostname}`}
           >
           </iframe>
-        </Section>
+        </Section>}
+
+        {showYoutubeChat &&<Section
+          title="YouTube Chat"
+          onHide={handleHide(setShowYoutubeChat)}
+        >
+          <iframe
+            className="h-full"
+            width="350"
+            src={`https://www.youtube.com/live_chat?v=${youtubeVideo}&embed_domain=${hostname}`}
+          >
+          </iframe>
+        </Section>}
+
       </div>
 
-      <Section
-        title="Twitch Chat"
-        show={showTwitchChat}
-        onHide={handleHide(setShowTwitchChat)}
-        onShow={handleShow(setShowTwitchChat)}
-      >
-        <iframe
-          className="h-full"
-          width="350"
-          src={`https://www.twitch.tv/embed/${twitchHandle}/chat?parent=${hostname}`}
-        >
-        </iframe>
-      </Section>
-
-      <Section
-        title="YouTube Chat"
-        show={showYoutubeChat}
-        onHide={handleHide(setShowYoutubeChat)}
-        onShow={handleShow(setShowYoutubeChat)}
-      >
-        <iframe
-          className="h-full"
-          width="350"
-          src={`https://www.youtube.com/live_chat?v=${youtubeVideo}&embed_domain=${hostname}`}
-        >
-        </iframe>
-      </Section>
+      <ul className="flex gap-1.5 px-2">
+        <li className="grow"></li>
+        {!showYoutubeVideo && <FooterButton title="YouTube Video" onClick={handleShow(setShowYoutubeVideo)} />}
+        {!showTwitchVideo && <FooterButton title="Twitch Video" onClick={handleShow(setShowTwitchVideo)} />}
+        {!showTwitchChat && <FooterButton title="Twitch Chat" onClick={handleShow(setShowTwitchChat)} />}
+        {!showYoutubeChat && <FooterButton title="YouTube Chat" onClick={handleShow(setShowYoutubeChat)} />}
+      </ul>
     </div>
   )
 }
